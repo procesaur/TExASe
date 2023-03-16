11e9ca3bea8a1828388cca860ae38c657cd91cf1
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2023-03-16 11:45:03 +0100

    **

--- rq_handler.py
+++ rq_handler.py
@@ -1,24 +1,71 @@
 from services import extract
 from helper import log_stuff
 from flask import request
+from os import path as px
 
 
 def process_req(req):
-    # get query parameters
+    query_parameters = params_from_req(req)
+
+    file, pdf = process_file_req(req, query_parameters)
+
+    lang = get_string(query_parameters, "lang")
+    renew = get_bool(query_parameters, "renew")
+
+    log_stuff([request.remote_addr, file, lang, renew])
+
+    function = extract
+    args = (file, lang, renew, pdf)
+
+    return function, args
+
+
+def params_from_req(req):
     query_parameters = req.args
     if len(query_parameters) == 0:
         query_parameters = req.form
+    return query_parameters
 
-    file = query_parameters.get('file')
-    lang = query_parameters.get('lang')
-    save_pdf = query_parameters.get('save_pdf')
 
-    log_stuff([request.remote_addr, file, lang, save_pdf])
+def process_file_req(req, query_parameters):
+    if "file" in req.files:
+        file = req.files["file"]
+        name = file.filename
+    else:
+        name = query_parameters.get('file')
+        if px.exists(name):
+            with open(name, "rb") as f:
+                file = f.read()
+        else:
+            file = None
 
-    if save_pdf == 1:
-        save_pdf = True
+    if name.endswith("pdf"):
+        pdf = True
+    else:
+        pdf = False
 
-    function = extract
-    args = (file, lang, save_pdf)
+    return file, pdf
 
-    return function, args
+
+def get_string(params, name, alternative=None):
+    try:
+        result = params.get(name)
+        if result is "":
+            result = alternative
+    except:
+        result = alternative
+
+    return result
+
+
+def get_bool(params, name, default=False):
+    try:
+        result = params.get(name)
+        if result == 1:
+            result = True
+        else:
+            result = False
+    except:
+        result = default
+
+    return result
--- services.py
+++ services.py
@@ -1,20 +1,35 @@
 from helper import cfg
-from textract import process
+from tika import parser
 from ocrmypdf import ocr
+from io import BytesIO
 
 
 def extract(args):
-    file, lang, save_pdf = args
+    file, lang, renew, pdf = args
 
-    if file.endswith(".pdf") and save_pdf:
-        try:
-            if lang is None and cfg["languages"]:
-                lang = "+".join(cfg["languages"])
-                ocr(input_file=file, output_file=file, skip_text=True, language=lang)
-            else:
-                ocr(input_file=file, output_file=file, skip_text=True)
-        except:
-            pass
+    if pdf:
+        output_file = ocr_file(file, lang)
+        if output_file and renew:
+            with open(file, "wb") as out_file:
+                out_file.write(output_file.getbuffer())
+        file = output_file
+    else:
+        output_file = None
 
-    text = process(file)
-    return text
+    text = parser.from_file(file)
+
+    return text, output_file
+
+
+def ocr_file(file, lang):
+    output = BytesIO()
+    try:
+        if lang is None and cfg["languages"]:
+            lang = "+".join(cfg["languages"])
+            ocr(input_file=file, output_file=output, skip_text=True, language=lang, deskew=True)
+        else:
+            ocr(input_file=file, output_file=output, skip_text=True)
+        return output
+
+    except:
+        return None
